159c83fbc8514bb32dbb743edd08728918f15322
DESKTOP-06HTJ9L\paly <pavol.valiska@gmail.com>
2025-05-14 12:56:13 +0200

    Update package.json to include postcss dependency; modify app.routes.ts to redirect to NunchakuManagerComponent directly.

--- nunchaki-table-manager/src/app/app.routes.ts
+++ nunchaki-table-manager/src/app/app.routes.ts
@@ -2,6 +2,5 @@ import { Routes } from '@angular/router';
 import { NunchakuManagerComponent } from './components/table-manager/table-manager.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'tables', pathMatch: 'full' },
-  { path: 'tables', component: NunchakuManagerComponent }
+  { path: '', component: NunchakuManagerComponent }
 ];
